fb133fd2af813b0dc5995998a37dcae34254de22
Justine <pignet.justine@gmail.com>
2020-05-10 16:10:16 +0200

    méthde show du wildcontroller

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,15 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild")
+ */
+
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="wild_index")
      */
     public function index() : Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Series',
         ]);
+    }
+
+    /**
+     * @Route("/show/{slug}",  name="wild_show", requirements={"slug"="[0-9-a-z]+$"})
+     */
+    public function show(string $slug = ""): Response
+    {
+        $title = self::toReadableTitle($slug);
+
+        return $this->render('wild/show.html.twig', ['slug' => $slug, 'title' => $title]);
+    }
+
+    public static function toReadableTitle($str) {
+        $strex = explode('-', $str);
+        for ($i = 0; $i < count($strex); $i++) {
+            $strex[$i] = ucfirst($strex[$i]);
+        }
+        $strim = implode(' ', $strex);
+        return $strim;
     }
 }
