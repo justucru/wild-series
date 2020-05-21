4723434087830e2ccda9bd4f63be3be578eed92c
Justine <pignet.justine@gmail.com>
2020-05-21 13:56:13 +0200

    quête 8 : findBy

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,9 +2,12 @@
 // src/Controller/WildController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Program;
+use App\Entity\Category;
 
 /**
  * @Route("/wild")
@@ -13,25 +16,98 @@ use Symfony\Component\Routing\Annotation\Route;
 Class WildController extends AbstractController
 {
     /**
+     * SHow all rows from Program's entity
+     *
      * @Route("/", name="wild_index")
+     * @return Response A response instance
      */
     public function index() : Response
     {
-        return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Series',
-        ]);
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program found in program\'s table.'
+            );
+        }
+
+        return $this->render(
+            'wild/index.html.twig',
+            ['programs' => $programs]
+        );
     }
 
     /**
-     * @Route("/show/{slug}",  name="wild_show", requirements={"slug"="[0-9-a-z]+$"})
+     * Getting a program with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     * @Route("/show/{slug<^[a-z0-9-]+$>}", defaults={"slug" = null}, name="wild_show")
+     * @return Response
      */
-    public function show(string $slug = ""): Response
+    public function show(?string $slug): Response
     {
-        $title = self::toReadableTitle($slug);
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
+        }
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with' . $slug . ' title found in program\'s table .'
+            );
+        }
 
-        return $this->render('wild/show.html.twig', ['slug' => $slug, 'title' => $title]);
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug,
+            'program' => $program]);
     }
 
+    /**
+     * Getting the list of programs by category
+     *
+     * @param string $categoryName
+     * @Route("/category/{categoryName}", name="show_category")
+     * @return Response
+     */
+    public function showByCategory(string $categoryName)
+    {
+        if (!$categoryName) {
+            throw $this
+                ->createNotFoundException('No category has been sent to find a category in category\'s table.');
+        }
+
+        $categoryId = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findBy(['name' => $categoryName]);
+
+        $programList = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(
+                ['category' => $categoryId],
+                ['id' => 'DESC'],
+                3
+            );
+
+
+        if (!$programList) {
+            throw $this->createNotFoundException(
+                'No program with ' . $categoryName . ' category found in program\'s table .'
+            );
+        }
+
+        return $this->render('wild/category.html.twig', [
+            'categoryName' => $categoryName,
+            'programList' => $programList]);
+    }
+    /*
     public static function toReadableTitle($str) {
         $strex = explode('-', $str);
         for ($i = 0; $i < count($strex); $i++) {
@@ -40,4 +116,5 @@ Class WildController extends AbstractController
         $strim = implode(' ', $strex);
         return $strim;
     }
+    */
 }
